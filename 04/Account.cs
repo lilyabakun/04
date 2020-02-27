122cd05ce1835e1d66e3403302f97e43c0046bbe
lilyabakun <lilya.bakun23@gmail.com>
2020-02-27 16:50:18 +0300

    Добавьте файлы проекта.

---
+++ 04/Account.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace _04
+{
+    class Account
+    {
+        string cur;
+        double summ;
+        public Account()
+        {
+            SetName("");
+
+            SetSumm(0.0);
+
+        }
+        public Account(string name, double summ)
+        {
+            SetName(name);
+            SetSumm(summ);
+        }
+
+        public void SetName(string Cur)
+        {
+            this.cur = Cur;
+        }
+        public void SetSumm(double Summ)
+        {
+            this.summ = Summ;
+        }
+        public void AddAccount(string c,double s)
+        {
+            Account acc = new Account();
+            acc.cur = c;
+            acc.summ = s;
+        }
+
+        public string GetCur()
+        {
+            return this.cur;
+
+        }
+        public double GetSumm()
+        {
+            return this.summ;
+
+        }
+        public override string ToString()
+        {
+            return string.Format("Сумма {0} Валюта{1}", this.GetSumm(), this.GetCur());
+        }
+        public Account Sum(Account a, Account b)
+        {
+            Account acc = new Account();
+            if (a.cur == b.cur)
+            {
+                acc.summ = a.summ + b.summ;
+            }
+            else return null;
+            return acc;
+        }   
+        public Account Sub(Account a, Account b)
+        {
+            Account acc = new Account();
+            if (a.cur == b.cur)
+            {
+                acc.summ = a.summ - b.summ;
+            }
+            else return null;
+            return acc;
+        }        
+        public Account Add(Account a, double s)
+        {
+            Account acc = new Account();
+            acc.summ = a.summ + s;
+            return acc;
+
+        }
+        public Account Proc(Account a)
+        {
+            Account acc = new Account();
+            acc.summ = a.summ + (a.summ * 0.05);
+            return acc;
+        }
+        
+        public static Account operator ++(Account a)
+        {
+            Account acc = new Account();
+            return acc.Proc(a);
+        }
+        public static Account operator +(Account a, Account b)
+        {
+            Account acc = new Account();
+            return acc.Sum(a, b);
+        }
+        public static bool operator <(Account a, Account b)
+        {
+            Account acc = new Account();
+            if (a.cur == b.cur)
+                return a.summ < b.summ;
+            return false;
+        }
+        public static bool operator >(Account a, Account b)
+        {
+            Account acc = new Account();
+            if (a.cur == b.cur)
+                return a.summ > b.summ;
+            return false;
+        }
+        public static Account operator +(Account a, double s)
+        {
+            Account acc = new Account();
+            return acc.Add(a, s);
+        }
+        public static Account operator -(Account a, Account b)
+        {
+            Account acc = new Account();
+            return acc.Sub(a, b);
+        }
+
+
+
+    }
+}
+
+
---
+++ 04/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1;
+            do
+            {
+                Console.Write("Меню\n1)Добавить счёт\n2)Слияние\n3)Снятие денег" +
+                    "\n4)Пополнение счёта\n5)Сравнение\n6)Проценты\n7)Выход\nЦифра:");
+                i1 = Convert.ToInt32(Console.ReadLine());
+                switch (i1)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите счёт");
+                            string c = Console.ReadLine();
+                            Console.WriteLine("Введите сумму");
+                            double s = Convert.ToDouble(Console.ReadLine());
+                            Account obj = new Account(c, s);
+
+                        }
+                        break;
+                    case 2:
+                        {
+
+                        }
+                        break;
+                    case 3:
+                        {
+
+                        }
+                        break;
+                    case 4:
+                        {
+
+                        }
+                        break;
+                    case 5:
+                        {
+
+                        }
+                        break;
+                    case 6:
+                        {
+
+                        }
+                        break;
+                    case 7:
+                        {
+
+                        }
+                        break;
+                }
+
+            } while (i1 != 6);
+            
+            Console.WriteLine("Введите счёт");
+            string c1 = Console.ReadLine();
+            Console.WriteLine("Введите сумму");
+            double s1 = Convert.ToDouble(Console.ReadLine());
+            Account obj1 = new Account(c1, s1);
+
+
+
+        }
+    }
+}
